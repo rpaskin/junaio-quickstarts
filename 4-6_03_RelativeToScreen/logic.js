c5fa936589e371f92b82f34cbf63317d63c60ff2
nicoKing <nicolas.king@metaio.com>
2012-12-13 11:14:11 +0100

    Changed RelativeToScreen Quickstart
    Changed Bullet Display

--- 4-6_03_RelativeToScreen/logic.js
+++ 4-6_03_RelativeToScreen/logic.js
@@ -5,14 +5,29 @@ arel.sceneReady(function()
 	//arel.Debug.deactivateArelLogStream();
 	
 	//Create the ammunition display
-	var ammunition = arel.Object.Model3D.createFromImage("ammo", "bullet3.jpg");
+	var ammunition1 = arel.Object.Model3D.createFromImage("ammo1", "bullet.png");
 	//Set the ScreenAnchor to Top-Right
-	ammunition.setScreenAnchor(arel.Constants.ANCHOR_TR);
-	ammunition.setTranslation(new arel.Vector3D(0,-40,0));
+	ammunition1.setScreenAnchor(arel.Constants.ANCHOR_TR);
+	ammunition1.setTranslation(new arel.Vector3D(0,-40,0));
 	//Add the new Object to the Scene
-	arel.Scene.addObject(ammunition);
+	arel.Scene.addObject(ammunition1);
+	
+	var ammunition2 = arel.Object.Model3D.createFromImage("ammo2", "bullet.png");
+	//Set the ScreenAnchor to Top-Right
+	ammunition2.setScreenAnchor(arel.Constants.ANCHOR_TR);
+	ammunition2.setTranslation(new arel.Vector3D(-20,-40,0));
+	//Add the new Object to the Scene
+	arel.Scene.addObject(ammunition2);
+	
+	var ammunition3 = arel.Object.Model3D.createFromImage("ammo3", "bullet.png");
+	//Set the ScreenAnchor to Top-Right
+	ammunition3.setScreenAnchor(arel.Constants.ANCHOR_TR);
+	ammunition3.setTranslation(new arel.Vector3D(-40,-40,0));
+	//Add the new Object to the Scene
+	arel.Scene.addObject(ammunition3);
 });
 
+var shots = 3;
 //This function will be triggered if the user is pressing the Shoot Button
 function shoot()
 {
@@ -22,21 +37,13 @@ function shoot()
 	object.startAnimation("fire", false);
 	
 	//Reduce the bullet display
-	var bulletDisplay = arel.Scene.getObject("ammo");
-	var bulletTex = bulletDisplay.getTexture();
+	if(shots >= 1)
+	{
+		var bullet = arel.Scene.getObject("ammo"+shots);
+		bullet.setTransparency(1);
+		shots--;
+	}
 	
-	if(bulletTex == "bullet3.jpg")
-	{
-		bulletDisplay.setTexture("bullet2.jpg");
-	}
-	else if(bulletTex == "bullet2.jpg")
-	{
-		bulletDisplay.setTexture("bullet1.jpg");
-	}
-	else if(bulletTex == "bullet1.jpg")
-	{
-		bulletDisplay.setTexture("bullet3.jpg");
-	}
 	
 	/*
 	  	To move the "Fadenkreuz" (engl. crosshair)use one of the constants
