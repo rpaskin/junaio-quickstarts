6324ec5dcd8a016c6ca0af737b22baf2ce348fc4
nicoKing <nicolas.king@metaio.com>
2012-12-13 10:27:21 +0100

    Changed RelativeToScreen Quickstart
    Changed from arel.xml to PHP
    Used the setAnchor function inside javascript
    Added Bullet Display via AREL

--- 4-6_03_RelativeToScreen/logic.js
+++ 4-6_03_RelativeToScreen/logic.js
@@ -3,6 +3,13 @@ arel.sceneReady(function()
 	//Just for Debuging purposes
 	//arel.Debug.activate();
 	//arel.Debug.deactivateArelLogStream();
+	
+	//Create the ammunition display
+	var ammunition = arel.Object.Model3D.createFromImage("ammo", "bullet3.jpg");
+	//Set the ScreenAnchor to Top-Right
+	ammunition.setScreenAnchor(arel.Constants.ANCHOR_TR);
+	//Add the new Object to the Scene
+	arel.Scene.addObject(ammunition);
 });
 
 //This function will be triggered if the user is pressing the Shoot Button
@@ -12,6 +19,23 @@ function shoot()
 	var object = arel.Scene.getObject("blaster");
 	//Start the shoot animation without loop
 	object.startAnimation("fire", false);
+	
+	//Reduce the bullet display
+	var bulletDisplay = arel.Scene.getObject("ammo");
+	var bulletTex = bulletDisplay.getTexture();
+	
+	if(bulletTex == "bullet3.jpg")
+	{
+		bulletDisplay.setTexture("bullet2.jpg");
+	}
+	else if(bulletTex == "bullet2.jpg")
+	{
+		bulletDisplay.setTexture("bullet1.jpg");
+	}
+	else if(bulletTex == "bullet1.jpg")
+	{
+		bulletDisplay.setTexture("bullet3.jpg");
+	}
 	
 	/*
 	  	To move the "Fadenkreuz" (engl. crosshair)use one of the constants
